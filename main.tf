a755534155ab8f13f9cb86246ec550f6f9f5e29a
Juliano Ribeiro Silva <julianorib@gmail.com>
2023-08-21 19:45:55 -0300

    Atualizando Projeto

--- main.tf
+++ main.tf
@@ -14,14 +14,14 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "resource_group" {
-  name     = var.nome
+  name     = "${var.nome}-rg"
   location = var.location
   tags     = local.common_tags
 }
 
 
 resource "azurerm_virtual_network" "virtual_network" {
-  name                = var.nome
+  name                = "${var.nome}-vnet"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = ["10.0.0.0/16"]
@@ -30,14 +30,14 @@ resource "azurerm_virtual_network" "virtual_network" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = var.nome
+  name                 = "${var.nome}-subnet-1"
   resource_group_name  = azurerm_resource_group.resource_group.name
   virtual_network_name = azurerm_virtual_network.virtual_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "public_ip" {
-  name                    = var.nome
+  name                    = "${var.nome}-publicIP"
   location                = azurerm_resource_group.resource_group.location
   resource_group_name     = azurerm_resource_group.resource_group.name
   allocation_method       = "Static"
@@ -47,12 +47,12 @@ resource "azurerm_public_ip" "public_ip" {
 }
 
 resource "azurerm_network_interface" "nic-vm1" {
-  name                = var.nome
+  name                = "${var.nome}-nic-vm1"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
 
   ip_configuration {
-    name                          = var.nome
+    name                          = "${var.nome}-ipconfig"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
@@ -62,7 +62,7 @@ resource "azurerm_network_interface" "nic-vm1" {
 }
 
 resource "azurerm_network_security_group" "nsg-vm1" {
-  name                = var.nome
+  name                = "${var.nome}-nsg-vm1"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
 
@@ -88,7 +88,7 @@ resource "azurerm_network_interface_security_group_association" "nic_to_nsg" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm1" {
-  name                  = var.nome
+  name                  = "${var.nome}-vm1"
   location              = azurerm_resource_group.resource_group.location
   resource_group_name   = azurerm_resource_group.resource_group.name
   network_interface_ids = [azurerm_network_interface.nic-vm1.id]
